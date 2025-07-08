a44e059f462e8f4cb358fd9e72b1c9cc64b74304
Carlos Vitor <cvitor.dev@gmail.com>
2025-07-08 16:25:23 -0300

    feat: project default files

---
+++ src/main/java/br/com/kbumwatcher/kbumwatcher/KbumWatcherApplication.java
@@ -0,0 +1,13 @@
+package br.com.kbumwatcher.kbumwatcher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KbumWatcherApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KbumWatcherApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/kbumwatcher/kbumwatcher/KbumWatcherApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.kbumwatcher.kbumwatcher;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KbumWatcherApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
