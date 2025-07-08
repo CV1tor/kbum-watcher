ad510ac9cc4882b93c1056c9acc61d8120bb1df2
Carlos Vitor <cvitor.dev@gmail.com>
2025-07-08 16:28:05 -0300

    feat: project exceptions

---
+++ src/main/java/br/com/kbumwatcher/kbumwatcher/exceptions/ProductException.java
@@ -0,0 +1,7 @@
+package br.com.kbumwatcher.kbumwatcher.exceptions;
+
+public class ProductException extends RuntimeException {
+    public ProductException(String message) {
+        super(message);
+    }
+}
---
+++ src/main/java/br/com/kbumwatcher/kbumwatcher/exceptions/ProductSearchException.java
@@ -0,0 +1,7 @@
+package br.com.kbumwatcher.kbumwatcher.exceptions;
+
+public class ProductSearchException extends RuntimeException {
+    public ProductSearchException(String message) {
+        super(message);
+    }
+}
